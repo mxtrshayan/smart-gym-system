1e1eb31ed48546b3449ec838d97a62f181914967
Shayan <mxtrshayan@gmail.com>
2026-01-22 11:04:06 +0500

    Fixed SSL issue for avien

--- db.js
+++ db.js
@@ -1,17 +1,21 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Database connection failed:", err);
+    console.error('Database connection failed:', err);
   } else {
-    console.log("Connected to MySQL Database");
+    console.log('Connected to MySQL database');
   }
 });
 
